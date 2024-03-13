c08757a9961d4974e552a0df0392e899426c5411
Aleksandr Vlasov <alexxx2001@gmail.com>
2024-03-13 13:27:33 +0700

    chore:
    nothing

--- routes/tools.php
+++ routes/tools.php
@@ -3,5 +3,4 @@
 use Topliner\ArtisanCommandExecutor\Http\Controllers\ArtisanExecuteCommandController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('api/tools/execute-command', [ArtisanExecuteCommandController::class, 'executeCommand']);
