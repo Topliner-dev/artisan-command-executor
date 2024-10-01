2cb26b941311b7595fd53e4594c44fe807240818
Aleksandr Vlasov <alexxx2001@gmail.com>
2024-10-01 19:00:18 +0700

    chore:
    php version has been changed

--- src/Services/ArtisanExecuteCommandService.php
+++ src/Services/ArtisanExecuteCommandService.php
@@ -18,7 +18,7 @@ class ArtisanExecuteCommandService
     public function executeCommand(array $validated): array
     {
         // убираем ограничение на время работы скрипта
-        ini_set('max_execution_time', 0);
+        ini_set('max_execution_time', '0');
 
         $pattern = '/^(\S+)(.*)/';
 
